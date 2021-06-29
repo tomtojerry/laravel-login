e6972aac3d35cccc5731ca0f4e233af6d944911d
tomtojerry <zayntom14@gmail.com>
2021-06-29 16:57:17 +0900

    create account lock

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -19,9 +21,77 @@ class AuthController extends Controller
 
     /**
      * @param App\Http\Requests\LoginFormRequest $request
+     * @return
      */
     public function login(LoginFormRequest $request) {
 
-        dd($request->all());
+        $credentials = $request->only('email', 'password');
+
+        // ①アカウントがロックされていたら弾く
+        $user = User::where('email', '=', $credentials['email'])->first();
+
+        if (!is_null($user)) {
+
+            if ($user->locked_flg === 1) {
+
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされています。'
+                ]);
+            }
+
+            if (Auth::attempt($credentials)) {
+
+                $request->session()->regenerate();
+
+                // ②成功したらエラーカウントを0にする
+                if ($user->error_count > 0) {
+
+                    $user->error_count = 0;
+
+                    $user->save();
+                }
+
+                return redirect()->route('home')->with('success', 'ログイン成功しました！');
+            }
+
+            // ③ログイン失敗したらエラーカウント1増やす
+            $user->error_count = $user->error_count + 1;
+
+            // ④エラーカウントが6以上の場合はアカウントをロックする
+            if ($user->error_count > 5) {
+
+                $user->locked_flg = 1;
+
+                $user->save();
+
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされました。解除したい場合は運営に連絡してください。'
+                ]);
+            }
+
+            $user->save();
+        }
+
+
+        return back()->withErrors([
+            'danger' => 'メールアドレスかパスワードが間違っています。'
+        ]);
+    }
+
+    /**
+     * ユーザーをアプリケーションからログアウトさせる
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('login.show')->with('danger', 'ログアウトしました！');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,23 @@ use App\Http\Controllers\Auth\AuthController;
 |
 */
 
-Route::get('/', [AuthController::class, 'showLogin'])->name('showLogin');
 
-Route::post('/login',[AuthController::class, 'login'])->name('login');
+Route::group(['middleware' => ['guest']], function () {
+
+    // ログインフォーム表示
+    Route::get('/', [AuthController::class, 'showLogin'])->name('login.show');
+
+    // ログイン処理
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+});
+
+Route::group(['middleware' => ['auth']], function () {
+
+    // ホーム画面
+    Route::get('home', function () {
+        return view('home');
+    })->name('home');
+
+    // ログアウト
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+});
