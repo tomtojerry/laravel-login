ae5e20840173534ec390254c0a028b0d6830b3c5
tomtojerry <zayntom14@gmail.com>
2021-06-30 12:03:30 +0900

    完成

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,68 @@ class User extends Authenticatable
     // protected $casts = [
     //     'email_verified_at' => 'datetime',
     // ];
+
+    /**
+     * Emailがマッチしたユーザーを返す
+     * @param string $email
+     * @return object
+     */
+    public function getUserByEmail($email) {
+
+        return User::where('email', '=', $email)->first();
+    }
+
+    /**
+     * エラーカウントをリセットする
+     * @param object $user
+     */
+    public function resetErrorCount($user)
+    {
+
+        if ($user->error_count > 0) {
+            $user->error_count = 0;
+            $user->save();
+        }
+    }
+
+    /**
+     * エラーカウントを1増やす
+     * @param int $error_count
+     * @return int
+     */
+    public function addErrorCount($error_count)
+    {
+        return $error_count + 1;
+    }
+
+    /**
+     * アカウントをロックする
+     * @param object $user
+     * @return bool
+     */
+    public function lockAccount($user)
+    {
+
+        if ($user->error_count > 5) {
+
+            $user->locked_flg = 1;
+
+            return $user->save();
+        }
+
+        return false;
+    }
+
+    /**
+     * アカウントがロックされているか？
+     * @param object $user
+     * @return bool
+     */
+    public function isAccountLocked($user) {
+
+        if ($user->locked_flg === 1) {
+            return true;
+        }
+        return false;
+    }
 }
